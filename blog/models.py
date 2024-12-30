6ba63e3503763eec4b20d3974a38ca551c861382
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-12-30 08:57:38 +0200

    Created a custom manager

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,17 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+# PublishedManager is a custom manager that retrieves all posts with the status of published
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super().get_queryset().filter(status=Post.Status.PUBLISHED))
+
+# Post model is a database table that stores blog posts
 class Post(models.Model):
-    class Status(models.TextChoices):
-        Draft = 'DF', 'Draft'
-        published = 'PB', 'Published'
+    # Status is a class that defines a set of choices for the status field
+    class Status(models.TextChoices): 
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
@@ -19,8 +26,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created  = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status, default=Status.Draft)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    objects = models.Manager() # the default manager
+    published = PublishedManager() # the custom manager
 
+    # metadata
     class Meta:
         # newest posts first
         ordering = ['-publish']
