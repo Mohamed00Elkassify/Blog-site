0bc8e5d05d253146ab61d14f5855286c67d26abd
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-02-06 23:52:10 +0200

    Created views and templates for post sharing and some styling

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 
 # post_list is a view that retrieves all published posts
@@ -40,6 +41,7 @@ def post_share(request, post_id):
         id = post_id,
         status = Post.Status.PUBLISHED
     )
+    sent = False
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -47,6 +49,24 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # send email
-        else:
-            form  = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post' : post, 'form' : form})
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you reading {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject = subject,
+                message = message, 
+                from_mail = None, 
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form  = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post' : post, 'form' : form, 'sent' : sent})
